22b915777e43a8233fea1a7a1aa5a8e7075ba93a
MohamedHesham2024 <mohmedhesham2024@gmail.com>
2026-04-10 15:18:05 +0200

    Disable server listening in index.js
    Comment out app.listen and console.log statements

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ mongoose.connect(process.env.MONGODBURI, {
 const app = require("./app")
 
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`App running on port ${process.env.PORT || 5000}`)
-});
+// app.listen(process.env.PORT || 5000, () => {
+//     console.log(`App running on port ${process.env.PORT || 5000}`)
+// });
+module.exports = app
